eab82942a3c92a54b00cef0731ba08d79e5935df
Shaun van Huyssteen <littleblueline@gmail.com>
2021-11-25 21:14:19 +0200

    Added in some static rendering for intial coffee stores

--- pages/index.js
+++ pages/index.js
@@ -3,8 +3,17 @@ import Image from "next/image";
 import styles from "../styles/Home.module.css";
 import Banner from "../components/banner/Banner";
 import Card from "../components/card";
+import coffeeStores from "../data/coffee-stores.json";
 
-export default function Home() {
+export async function getStaticProps(context) {
+  return {
+    props: {
+      coffeeStores,
+    }, // will be passed to the page component as props
+  };
+}
+
+export default function Home(props) {
   const handleOnBannerButtonClick = () => {
     console.log("hi banner button");
   };
@@ -27,18 +36,17 @@ export default function Home() {
           />
         </div>
         <div className={styles.cardLayout}>
-          <Card
-            name='DarkHorse Coffee'
-            imgUrl='/static/hero-image.png'
-            href='/coffee-store/darkhorse-coffee'
-            className={styles.card}
-          />
-          <Card
-            name='DarkHorse Coffee'
-            imgUrl='/static/hero-image.png'
-            href='/coffee-store/darkhorse-coffee'
-            className={styles.card}
-          />
+          {props.coffeeStores.map((coffeeStore) => {
+            return (
+              <Card
+                key={coffeeStore.id}
+                name={coffeeStore.name}
+                imgUrl={coffeeStore.imgUrl}
+                href={`/coffee-store/${coffeeStore.id}`}
+                className={styles.card}
+              />
+            );
+          })}
         </div>
       </main>
     </div>
